736ff03ac637bd965a7c16345262518823e7cbe4
jj <darkarcmage@gmail.com>
2019-03-04 13:49:15 +0200

    setup

--- src/index.js
+++ src/index.js
@@ -5,12 +5,29 @@ export default (editor, opts = {}) => {
   const options = { ...{
     // default options
   },  ...opts };
-
+	var blockManager = editor.BlockManager;
   // Add components
   loadComponents(editor, options);
 
   // Add blocks
+  
+  blockManager.add('HTML_LINK',{
+	  id:'HTML_LINK',
+	  label:'HTML Link',
+	  content:{
+		  content:'<div class="my-block">This is a simple block</div>',
+		  type:'HTML_LINK'
+
+	  },
+  });
+  
+  
   loadBlocks(editor, options);
+  
+  
+  
+  
+
 
   // TODO Remove
   editor.on('load', () => editor.addComponents(`<div style="margin:100px; padding:25px;">Content loaded from the plugin</div>`, { at: 0 }))
